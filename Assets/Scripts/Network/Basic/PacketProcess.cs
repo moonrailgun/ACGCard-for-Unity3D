f5bc96cfe92e8db457046410392bba528de60221
moonrailgun <moonrailgun@gmail.com>
2015-03-15 20:35:41 +0800

    登陆跳转
    ADD 数据包处理类
    UPDATE 登陆成功后跳转到主界面

---
+++ Assets/Scripts/Network/Basic/PacketProcess.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PacketProcess
+{
+    public void Process(string text)
+    {
+        string[] arguments = text.Split(new char[] { ' ' });
+        if (arguments[1] == "login")
+        {
+            if (arguments[2] == "true") { LoginPacketProcess(true); }
+            else { LoginPacketProcess(false); }
+        }
+    }
+
+    private void LoginPacketProcess(bool isAccess)
+    {
+        if (isAccess)
+        {
+            ShortMessagesSystem.Instance.ShowShortMessage("登陆成功");
+            Application.LoadLevel("MenuScene");//载入主场景
+        }
+        else
+        {
+            ShortMessagesSystem.Instance.ShowShortMessage("登陆失败");
+        }
+    }
+}
