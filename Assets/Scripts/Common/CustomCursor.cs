bd95a5afbd83ca26c6105b65c50692cddff4fcdc
moonrailgun <moonrailgun@gmail.com>
2015-03-27 19:39:52 +0800

    自定义鼠标指针
    ADD 自制鼠标指针

---
+++ Assets/Scripts/Common/CustomCursor.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CustomCursor : MonoBehaviour
+{
+    public Texture2D cursor;
+    public Vector2 hotspot = new Vector2(0,5);
+
+    private void Awake()
+    {
+        Cursor.SetCursor(cursor, hotspot, CursorMode.ForceSoftware);
+    }
+}
