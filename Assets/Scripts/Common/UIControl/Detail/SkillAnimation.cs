f2852227ee74146ab25d2716f6db6415745e5d04
moonrailgun <moonrailgun@gmail.com>
2015-04-22 21:20:53 +0800

    帧动画与示例
    ADD 帧动画系统
    ADD 爆炸特效

---
+++ Assets/Scripts/Common/UIControl/Detail/SkillAnimation.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkillAnimation : MonoBehaviour
+{
+    public int spriteNum = 10;
+    public string format = "image {0}";
+    public float intervalSecond = 0.5f;//每帧间隔时间
+
+    private void Start()
+    {
+        Play();
+    }
+
+    public void Play()
+    {
+        StartCoroutine("AnimationCoroutine");
+    }
+    public void Play(int num, string format, float intervalSecond)
+    {
+        this.spriteNum = num;
+        this.format = format;
+        this.intervalSecond = intervalSecond;
+        this.Play();
+    }
+
+    private IEnumerator AnimationCoroutine()
+    {
+        UISprite sprite = GetComponent<UISprite>();
+        for (int i = 0; i < spriteNum; i++)
+        {
+            sprite.spriteName = string.Format(format, i);
+            sprite.MakePixelPerfect();
+            yield return new WaitForSeconds(intervalSecond);
+        }
+        yield return 0;
+    }
+}
