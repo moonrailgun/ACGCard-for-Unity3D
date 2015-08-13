678ef4113540c72f47725cb3617d774cc0527f17
moonrailgun <moonrailgun@gmail.com>
2015-08-13 21:46:27 +0800

    道具管理器与道具JSON数据
    ADD 道具卡片管理器框架
    ADD 1~7号道具JSON数据

---
+++ Assets/Scripts/Common/Card/ItemCardManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class ItemCardManager
+{
+    #region 单例模式
+    private static ItemCardManager _instance;
+    public static ItemCardManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new ItemCardManager();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public void ReadItemJsonDate()
+    {
+
+    }
+}
