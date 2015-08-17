d290a4b725935f89688ac5d1626be46d3c6e74bc
moonrailgun <moonrailgun@gmail.com>
2015-08-17 16:20:11 +0800

    英雄界面UI
    ADD 英雄界面UI设计
    ADD Tiny尺寸卡片
    ADD 分割线

--- Assets/Scripts/Common/UIControl/StartScene.cs
+++ Assets/Scripts/Common/UIControl/StartScene.cs
@@ -4,6 +4,8 @@ public class StartScene : MonoBehaviour
 {
     void Start()
     {
+        ItemCardManager.Instance.ReadItemJsonDate();
+        return;
         LogsSystem.Instance.Print("游戏开始");
         Application.LoadLevel("LoginScene");
     }
